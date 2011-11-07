a2323b5cd76d81ef02ac4df2812a1fc5bce47298
Fred Strauss <fred@strider.co.za>
2011-11-07 20:17:13 +0200

    Added GPL license info

--- pokerev.c
+++ pokerev.c
@@ -1,10 +1,24 @@
 /*
  * pokerev.c
- * Copyright 2011 Fred Strauss
  *
  * Calculates Poker EV
  *
- * TODO Add GPL license here
+ * Copyright 2011 Fred Strauss
+ *
+ * This program is distributed under the terms of the GNU General Public License.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
